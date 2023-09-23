7e6b9f27b792b128d31d05cf902631838d847383
Vipul Goyal <vipulgoyal006@gmail.com>
2023-09-23 09:43:17 +0530

    First Commit

---
+++ src/components/pages/Home.js
@@ -0,0 +1,19 @@
+import '../../App.css';
+import Cards from '../Cards';
+import Footer from '../Footer';
+import HeroSection  from '../HeroSection';
+import React from 'react';
+
+
+
+function Home () {
+    return (
+       <>
+       <HeroSection />
+       <Cards />
+       <Footer />
+       </> 
+    );
+}
+
+export default Home;
